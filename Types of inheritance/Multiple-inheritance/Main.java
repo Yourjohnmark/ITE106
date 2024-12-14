9325c2473cf518bff9e2dd95e8476762bc7fb538
Johnmarkcapistrano <johnmarkcapistrano53@gmail.com>
2024-12-14 12:08:30 +0800

    Create Main.java

---
+++ Types of inheritance/Multiple-inheritance/Main.java
@@ -0,0 +1,10 @@
+// Drived class
+public class Main {
+    public static void main(String[] args)
+    {
+        child c = new child();
+        c.print_geek();
+        c.print_for();
+        c.print_geek();
+    }
+}
